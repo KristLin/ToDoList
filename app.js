a223f0956a298a06a4f448b6bf64150301f3fca5
KristLin <kristlin.unsw@gmail.com>
2019-09-04 15:44:24 +1000

    Add Angela's css file and modify web page

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.set('view engine', 'ejs');
 var things = ["Learn Web Development", "Workout", "Shopping"];
 
 app.get("/", function(req, res) {
-  res.render('index', {title: getTitle(), things: things});
+  res.render('index', {date: getDate(), things: things});
 });
 
 app.post("/", function(req, res) {
-  let newThing = req.body.newThing;
-  things.push(newThing);
-  res.render("index", {title: getTitle(), things: things});
+  let newItem = req.body.newItem;
+  things.push(newItem);
+  res.render("index", {date: getDate(), things: things});
 });
 
 app.listen(3000, function() {
@@ -24,14 +24,7 @@ app.listen(3000, function() {
 });
 
 
-function getTitle() {
-  let today = new Date().getDay();
-  let dateList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let title_result = "";
-  if (today == 6 || today == 0) {
-    titleResult = "Todya is " + dateList[today] + "! Yeaaaah!";
-  } else {
-    titleResult = "Todya is " + dateList[today] + ".";
-  }
-  return titleResult;
+function getDate() {
+  let date = new Date().toDateString().split(" ");
+  return date[0] + ", " + date[1] + " " + date[2] + ", " + date[3];
 }
